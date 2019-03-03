4c39c2a115fe92d1479600cc4453bced4c4c7b5b
gera1013 <gera1013@users.noreply.github.com>
2019-03-03 14:02:53 -0600

    Cambios en display

--- HojaDeTrabajo6/src/hojadetrabajo6/Tree.java
+++ HojaDeTrabajo6/src/hojadetrabajo6/Tree.java
@@ -171,12 +171,11 @@ public class Tree<K,V> extends AbstractMap<K,V>
     @Override
     public void display()
     {
-        Set set;
-        Iterator iterator;
-        set = datos.entrySet();
-        iterator = set.iterator();
-        while(iterator.hasNext()){
-            System.out.println(iterator.next());
-        }
+        Set<K> set = datos.keySet();
+        set.forEach((name) -> {
+            String key = name.toString();
+            String value = datos.get(name).toString();
+            System.out.println("Carta: " + key + " / Tipo: " + value);
+        });
     }
 }
